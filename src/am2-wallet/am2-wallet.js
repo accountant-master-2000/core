5e11129fa7a87ab185bb960da7162116b3ed4ee5
Rafael 'fumasa' Giorgetti <rafael@wakedown.org>
2018-03-31 20:18:33 -0300

    removing shadow dom

--- src/am2-wallet/am2-wallet.js
+++ src/am2-wallet/am2-wallet.js
@@ -1,51 +1,50 @@
-class AM2Wallet extends window.HTMLElement {
-  constructor () {
-    super()
+(function () {
+  class AM2Wallet extends window.HTMLElement {
+    constructor () {
+      super()
 
-    this.addEventListener('click', this._onClick)
-  }
-
-  get storeId () { return this.getAttribute('store-id') }
-  get storeTagName () {
-    let store = this
-    while (store.parentNode) {
-      store = store.parentNode
+      this.addEventListener('click', this._onClick)
     }
-    store = store.querySelector(`#${this.storeId}`)
-    if (store === null) return ''
-    return store.tagName.toLowerCase()
-  }
 
-  get id () { return this.getAttribute('id') }
+    get storeId () { return this.getAttribute('store-id') }
+    get storeTagName () {
+      let store = this
+      while (store.parentNode) {
+        store = store.parentNode
+      }
+      store = store.querySelector(`#${this.storeId}`)
+      if (store === null) return ''
+      return store.tagName.toLowerCase()
+    }
 
-  set data (value) { window.localStorage.setItem(`${this.storeTagName}-data-${this.storeId}`, JSON.stringify(value)) }
-  get data () {
-    if (this.storeTagName !== '') {
-      return JSON.parse(window.localStorage.getItem(`${this.storeTagName}-data-${this.storeId}`)).wallets.find((v) => v.alias === this.id)
-    } else {
-      return { alias: this.id, lastUpdate: new Date(), transactions: [] }
+    get id () { return this.getAttribute('id') }
+
+    set data (value) { window.localStorage.setItem(`${this.storeTagName}-data-${this.storeId}`, JSON.stringify(value)) }
+    get data () {
+      if (this.storeTagName !== '') {
+        return JSON.parse(window.localStorage.getItem(`${this.storeTagName}-data-${this.storeId}`)).wallets.find((v) => v.alias === this.id)
+      } else {
+        return { alias: this.id, lastUpdate: new Date(), transactions: [] }
+      }
+    }
+
+    connectedCallback () {
+      const template = document.currentScript.ownerDocument.querySelector('#am2-wallet-template')
+      const instance = template.content.cloneNode(true)
+      this.appendChild(instance)
+
+      this.querySelector('.am2-wallet-title').innerHTML = `${this.data.alias}: ${this.data.lastUpdate}`
+      this.querySelector('.am2-wallet-transactions').innerHTML = JSON.stringify(this.data.transactions)
+    }
+
+    disconnectedCallback () {
+      this.removeEventListener('click', this._onClick)
+    }
+
+    _onClick (event) {
+      console.log('onClick', event.target)
     }
   }
 
-  connectedCallback () {
-    if (this.shadowRoot !== null) return
-    const shadowRoot = this.attachShadow({ mode: 'open' })
-
-    const template = document.currentScript.ownerDocument.querySelector('#am2-wallet-template')
-    const instance = template.content.cloneNode(true)
-    shadowRoot.appendChild(instance)
-
-    this.shadowRoot.querySelector('.am2-wallet-title').innerHTML = `${this.data.alias}: ${this.data.lastUpdate}`
-    this.shadowRoot.querySelector('.am2-wallet-transactions').innerHTML = JSON.stringify(this.data.transactions)
-  }
-
-  disconnectedCallback () {
-    this.removeEventListener('click', this._onClick)
-  }
-
-  _onClick (event) {
-    this._selectWallet(event.target)
-  }
-}
-
-if (!window.customElements.get('am2-wallet')) window.customElements.define('am2-wallet', AM2Wallet)
+  if (!window.customElements.get('am2-wallet')) window.customElements.define('am2-wallet', AM2Wallet)
+})()
--- src/am2-wallets/am2-wallets.js
+++ src/am2-wallets/am2-wallets.js
@@ -1,132 +1,89 @@
-class AM2Wallets extends window.HTMLElement {
-  connectedCallback () {
-    const shadowRoot = this.attachShadow({ mode: 'open' })
+(function () {
+  class AM2Wallets extends window.HTMLElement {
+    connectedCallback () {
+      const template = document.currentScript.ownerDocument.querySelector('#am2-wallets-template')
+      const instance = template.content.cloneNode(true)
+      this.appendChild(instance)
 
-    const template = document.currentScript.ownerDocument.querySelector('#am2-wallets-template')
-    const instance = template.content.cloneNode(true)
-    shadowRoot.appendChild(instance)
+      console.log('allWallets', JSON.stringify(this._allWallets()))
+      console.log('allAccounts', JSON.stringify(this._allAccounts()))
+      console.log('allCategories', JSON.stringify(this._allCategories()))
+    }
 
-    this.addEventListener('click', this._onClick)
+    disconnectedCallback () {
+      this.removeEventListener('click', this._onClick)
+    }
 
-    console.log('allAccounts', JSON.stringify(this._allAccounts()))
-    console.log('allCategories', JSON.stringify(this._allCategories()))
-
-    Promise.all([
-      window.customElements.whenDefined('am2-github-gist'),
-      window.customElements.whenDefined('am2-wallet')
-    ]).then(_ => {
-      const store = this._store()
-      if (store !== undefined) this.shadowRoot.querySelector('.am2-wallets-content').appendChild(store)
-      this._allWallets().forEach((val, idx) => {
-        this.shadowRoot.querySelector('.am2-wallets-content').appendChild(val)
-      })
-    })
-  }
-
-  disconnectedCallback () {
-    this.removeEventListener('click', this._onClick)
-  }
-
-  get data () {
-    let store = this._store()
-    if (store === undefined) {
-      store = this
-      while (store.parentNode) {
-        store = store.parentNode
+    get data () {
+      let store = this._store()
+      if ((store !== undefined) && (store.tagName !== '') && store.getAttribute('id') !== undefined) {
+        const data = JSON.parse(window.localStorage.getItem(`${store.tagName.toLowerCase()}-data-${store.getAttribute('id')}`))
+        return data
+      } else {
+        return null
       }
-      store = Array.from(store.querySelectorAll('am2-github-gist'))[0]
     }
-    if ((store !== undefined) && (store.tagName !== '') && store.getAttribute('id') !== undefined) {
-      const data = JSON.parse(window.localStorage.getItem(`${store.tagName.toLowerCase()}-data-${store.getAttribute('id')}`))
-      return data
-    } else {
-      return null
+
+    _store () {
+      let gist = Array.from(this.querySelectorAll('am2-github-gist'))
+      if (gist.length === 0) {
+        gist = this
+        while (gist.parentNode) {
+          gist = gist.parentNode
+        }
+        gist = Array.from(gist.querySelectorAll('am2-github-gist'))
+      }
+      return gist[0]
     }
-  }
 
-  _store () {
-    const gist = Array.from(this.querySelectorAll('am2-github-gist'))
-    // console.log('store', gist, gist[0])
-    return gist[0]
-  }
-
-  _allAccounts () {
-    const data = this.data
-    if (data !== null) {
-      let newArr = [ 'me' ]
-      data.wallets.forEach((w) => {
-        w.transactions.map((t) => (t.from !== 'me' ? t.from : t.to)).forEach(a => {
-          if (newArr.indexOf(a) === -1) newArr.push(a)
-        })
-      })
-      return newArr
-    } else {
-      return []
-    }
-  }
-
-  _allCategories () {
-    const data = this.data
-    if (data !== null) {
-      let newArr = []
-      data.wallets.forEach((w) => {
-        let newNewArr = []
-        w.transactions.forEach((t) => {
-          t.categories.forEach((c) => {
-            if (newNewArr.indexOf(c) === -1 && newArr.indexOf(c) === -1) newNewArr.push(c)
+    _allAccounts () {
+      const data = this.data
+      if (data !== null) {
+        let newArr = [ 'me' ]
+        data.wallets.forEach((w) => {
+          w.transactions.map((t) => (t.from !== 'me' ? t.from : t.to)).forEach(a => {
+            if (newArr.indexOf(a) === -1) newArr.push(a)
           })
         })
-        newArr = newArr.concat(newNewArr)
-      })
-      return newArr
-    } else {
-      return []
+        return newArr
+      } else {
+        return []
+      }
+    }
+
+    _allCategories () {
+      const data = this.data
+      if (data !== null) {
+        let newArr = []
+        data.wallets.forEach((w) => {
+          let newNewArr = []
+          w.transactions.forEach((t) => {
+            t.categories.forEach((c) => {
+              if (newNewArr.indexOf(c) === -1 && newArr.indexOf(c) === -1) newNewArr.push(c)
+            })
+          })
+          newArr = newArr.concat(newNewArr)
+        })
+        return newArr
+      } else {
+        return []
+      }
+    }
+
+    _allWallets () {
+      const data = this.data
+      if (data !== null) {
+        return data.wallets.map(w => (w.alias))
+      } else {
+        return []
+      }
+    }
+
+    _allDisplayWallets () {
+      const walletList = Array.from(this.querySelectorAll('am2-wallet'))
+      return walletList
     }
   }
 
-  _allWallets () {
-    const walletList = Array.from(this.querySelectorAll('am2-wallet'))
-    // console.log('allWallets', walletList)
-    return walletList
-  }
-
-  _prevWallet () {
-    const wallets = this._allWallets()
-    let newIdx = wallets.findIndex(wallet => wallet.selected) - 1
-    return wallets[(newIdx + wallets.length) % wallets.length]
-  }
-
-  _firstWallet () {
-    const wallets = this._allWallets()
-    return wallets[0]
-  }
-
-  _lastWallet () {
-    const wallets = this._allWallets()
-    return wallets[wallets.length - 1]
-  }
-
-  _nextWallet () {
-    const wallets = this._allWallets()
-    let newIdx = wallets.findIndex(wallet => wallet.selected) + 1
-    return wallets[newIdx % wallets.length]
-  }
-
-  reset () {
-    const wallets = this._allWallets()
-    wallets.forEach(wallet => { wallet.selected = false })
-  }
-
-  _selectWallet (newWallet) {
-    if (newWallet === undefined) return
-    this.reset()
-    newWallet.selected = true
-    newWallet.focus()
-  }
-
-  _onClick (event) {
-    this._selectWallet(event.target)
-  }
-}
-
-if (!window.customElements.get('am2-wallets')) window.customElements.define('am2-wallets', AM2Wallets)
+  if (!window.customElements.get('am2-wallets')) window.customElements.define('am2-wallets', AM2Wallets)
+})()
